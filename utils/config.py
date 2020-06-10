544e793ea27d70f091e06195f07364aeb1aef4e4
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-06-09 21:54:58 -0400

    Implemented glueware and MS integration Implemented glueware and MS integration using Python. However, it is to be acknowledged that this implementation is of little extensibility, and should be improved.

---
+++ utils/config.py
@@ -0,0 +1,13 @@
+""" Config for metasmoke and tokenizers. """
+
+
+class Config:
+    """ Config here. """
+    ms_key = "your_ms_key_here"
+    ms_host = "ms_host_here"
+    ms_ws_host = "ms_websocket_host_here"
+    MS_WS_MAX_RETRIES = 5  # Max retries before giving up
+
+    naa_bias = 0.5  # Change to whatever you like as long as 0 <= naa_bias <= 1
+    un_thres = 2  # Threshold for unanimous feedbacks
+    co_thres = 3  # Ratio threshold for controversial feedbacks
