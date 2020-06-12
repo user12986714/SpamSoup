fdb5e49b95fc4fc11daf4519beb3db260c5d7784
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-06-12 15:41:02 -0400

    ms.py: Show users more information

--- utils/ms.py
+++ utils/ms.py
@@ -82,8 +82,10 @@ def ms_ws_listener():
             data = json.loads(resp)
 
             if "type" in data:
+                if data["type"] == "welcome":
+                    print("Successfully connected to MS WebSocket.\n")
                 if data["type"] == "reject_subscription":
-                    raise RuntimeError("MS WS connection rejected.")
+                    raise RuntimeError("MS WS connection rejected.\n")
                 if data["type"] == "ping":
                     continue
 
@@ -93,6 +95,7 @@ def ms_ws_listener():
             msg = data["message"]
             if msg["event_class"] == "Post":
                 # New post created. Analyze it.
+                print("New post {} received from WebSocket.\n".format(msg["object"]["id"]))
                 user_link = msg["object"]["user_link"]
                 user_re = re.search(r'^(?:https?:)?\/\/([a-z.]+)\/users\/([0-9]+)', user_link)
 
@@ -100,22 +103,32 @@ def ms_ws_listener():
                         msg["object"]["username"])
 
                 post_tuple = (msg["object"]["title"], msg["object"]["body"], user)
-                analyze_post(post_tuple)
+                analyze_post(msg["object"]["id"], post_tuple)
 
             if msg["event_class"] == "Feedback":
                 # Updates on feedback. Check if over threshold.
                 post_id = msg["object"]["post_id"]
+                print("New feedback event on post {} received from WebSocket.\n".format(post_id))
+
                 time.sleep(1)  # This is needed due to an issue in MS API.
 
                 feedbacks = get_feedback_on_post(post_id)
+                # No empty line between the following two prints.
+                print("List of Feedbacks on post {} fetched from HTTP.".format(post_id))
+                print(feedbacks)
+                print("")  # Print an empty after feedbacks.
+
                 is_over_thres = feedback_over_threshold(feedbacks)
                 if is_over_thres is None:
                     # Not yet.
+                    print("Feedbacks on post {} are insufficient.\n".format(post_id))
                     continue
+                print("Post {} registered as {}\n".format(post_id, "tp" if is_over_thres else "fp"))
 
                 # Fetch post to be learned.
                 post_tuple = get_post(post_id)
-                learn_post(post_tuple, is_over_thres)
+                print("Post {} fetched from HTTP.\n".format(post_id))
+                learn_post(post_id, post_tuple, is_over_thres)
         except RuntimeError:
             # Severe errors
             raise
@@ -192,7 +205,7 @@ def naive_tokenizer(post_tuple):
 
 # Ad hoc code ahead.
 # Please consider improving the following code to make them extensible.
-def analyze_post(post_tuple):
+def analyze_post(post_id, post_tuple):
     """ Call appropriate executables to analyze the post. """
     tokenized_post = naive_tokenizer(post_tuple)
     tokenized_post_str = "\n".join(tokenized_post) + "\n"
@@ -217,16 +230,17 @@ def analyze_post(post_tuple):
     bow.stdin.write(tokenized_post_bytes)
     bow.stdin.close()
 
-    sbph_nbc_out = sbph_nbc.communicate()[0].decode("utf-8")[0]
-    ngram_nbc_out = ngram_nbc.communicate()[0].decode("utf-8")[0]
-    bow_nbc_out = bow_nbc.communicate()[0].decode("utf-8")[0]
+    sbph_nbc_out = sbph_nbc.communicate()
+    ngram_nbc_out = ngram_nbc.communicate()
+    bow_nbc_out = bow_nbc.communicate()
 
-    print("Classify post: NT-SBPH-NBC: {}; NT-Ngram-NBC: {}; NT-BoW-NBC: {}".format(sbph_nbc_out,
-                                                                                    ngram_nbc_out,
-                                                                                    bow_nbc_out))
+    print("Post {} classified as: ".format(post_id))
+    print( "NT-SBPH-NBC: {}; ".format(sbph_nbc_out))
+    print( "NT-Ngram-NBC: {}; ".format(ngram_nbc_out))
+    print("NT-BoW-NBC: {}\n".format(bow_nbc_out))
 
 
-def learn_post(post_tuple, is_tp):
+def learn_post(post_id, post_tuple, is_tp):
     """ Call appropriate executables to learn the post. """
     learn_arg_str = "--learn={}".format("T" if is_tp else "F")
 
@@ -253,13 +267,14 @@ def learn_post(post_tuple, is_tp):
     bow.stdin.write(tokenized_post_bytes)
     bow.stdin.close()
 
-    sbph_nbc_out = sbph_nbc.communicate()[0].decode("utf-8")[0]
-    ngram_nbc_out = ngram_nbc.communicate()[0].decode("utf-8")[0]
-    bow_nbc_out = bow_nbc.communicate()[0].decode("utf-8")[0]
+    sbph_nbc_out = sbph_nbc.communicate()
+    ngram_nbc_out = ngram_nbc.communicate()
+    bow_nbc_out = bow_nbc.communicate()
 
-    print("Learn post: NT-SBPH-NBC: {}; NT-Ngram-NBC: {}; NT-BoW-NBC: {}".format(sbph_nbc_out,
-                                                                                 ngram_nbc_out,
-                                                                                 bow_nbc_out))
+    print("Post {} learned as: ".format(post_id))
+    print("NT-SBPH-NBC: {}; ".format(sbph_nbc_out))
+    print("NT-Ngram-NBC: {}; ".format(ngram_nbc_out))
+    print("NT-BoW-NBC: {}\n".format(bow_nbc_out))
 
 
 # Ad hoc code ends here
