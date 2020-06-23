05dd06b201cdad0c58d258dd48183dd14e8fec1d
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-06-22 23:05:39 -0400

    build.sh: Adjust build script accordingly

--- build.sh
+++ build.sh
@@ -5,12 +5,8 @@ if [ -d "bin" ]; then
 fi
 
 mkdir bin
-mkdir bin/tk
 mkdir bin/vc
 mkdir bin/cf
-
-cp src/tk/nt.py bin/tk/nt.py
-cp src/tk/stopword.py bin/tk/stopword.py
 
 cc src/vc/sbph.c src/vc/vcutils.c -Wall -o bin/vc/sbph
 cc src/vc/ngram.c src/vc/vcutils.c -Wall -o bin/vc/ngram
@@ -18,10 +14,15 @@ cc src/vc/bow.c src/vc/vcutils.c -Wall -o bin/vc/bow
 
 cc src/cf/nbc.c src/cf/cfutils.c -lm -Wall -o bin/cf/nbc
 
-cp src/ms.py bin/ms.py
-cp src/config.json bin/config.json
-cp src/verinfo.json bin/verinfo.json
+cp src/cfgparse.py bin/cfgparse.py
+cp src/dataproc.py bin/dataproc.py
+cp src/glue.py bin/glue.py
+cp src/ml.py bin/ml.py
+cp src/msapi.py bin/msapi.py
+cp src/msg.py bin/msg.py
+cp src/stopword.py bin/stopword.py
+cp src/verinfo.py bin/verinfo.py
 
 chmod +x utils/mknbcdat.sh
 
-chmod +x bin/ms.py
+chmod +x bin/glue.py
