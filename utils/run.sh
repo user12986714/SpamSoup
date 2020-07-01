e0f585195f59585af641051117ef1184db79511c
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-07-01 10:34:33 -0400

    ws_tee.py: Send stdout through WebSocket

---
+++ utils/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh
+
+wait_until_ready(){
+    while ! [ -f "READY" ]; do
+        sleep 5
+    done
+}
+
+rescue_docker(){
+    touch "WARNING"
+    sleep infinity
+}
+
+wait_until_ready
+python3 -u ./bin/glue.py --config=cfg.json | stdbuf -o0 python3 ws_tee.py
+rescue_docker
