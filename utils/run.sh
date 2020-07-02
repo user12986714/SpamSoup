cd3e4c4334d72082334c5214800b001a923eb66c
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-07-02 15:26:35 -0400

    run.sh: Auto upload data file

--- utils/run.sh
+++ utils/run.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/sh
 
+ON_BRANCH="prod"
+DATA_BRANCH="instance-name"
+
 wait_until_ready(){
     while ! [ -f "READY" ]; do
         sleep 5
     done
 }
 
-rescue_docker(){
-    touch "WARNING"
-    sleep infinity
-}
-
 wait_until_ready
-python3 -u ./bin/glue.py --config=cfg.json --stopword=sw.json | stdbuf -o0 python3 ws_tee.py
-rescue_docker
+while :; do
+    git pull upstream "${ON_BRANCH}"
+    git checkout "${ON_BRANCH}"
+    sh build.sh
+    python3 -u ./bin/glue.py --config=cfg.json --stopword=sw.json | stdbuf -o0 python3 ws_tee.py
+    cd data
+    if ! git branch | grep "${DATA_BRANCH}"; then
+        git checkout master
+        git branch "${DATA_BRANCH}"
+    fi
+    git checkout "${DATA_BRANCH}"
+    git add .
+    git commit -m "$(date +%s)"
+    git push
+    cd ..
+done
