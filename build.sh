1879d59e59639ecf15b83523d725323eb80c93f6
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-08-06 10:35:43 -0400

    build.sh: Use CC and CCFLAGS + enable -O3 -Ofast

--- build.sh
+++ build.sh
@@ -7,16 +7,19 @@ if [ -d "bin" ]; then
 fi
 mkdir -p bin/vc bin/cf
 
+CC="cc"
+CCFLAGS="-Wall -O3 -Ofast"
+
 # Please don't try to concatenate these lines,
 # as those source code may have different dependencies
-cc src/vc/sbph.c src/vc/vcutils.c -Wall -o bin/vc/sbph
-cc src/vc/ngram.c src/vc/vcutils.c -Wall -o bin/vc/ngram
-cc src/vc/bow.c src/vc/vcutils.c -Wall -o bin/vc/bow
-cc src/vc/osb.c src/vc/vcutils.c -Wall -o bin/vc/osb
-cc src/vc/sbphmh.c src/vc/vcutils.c -Wall -o bin/vc/sbphmh
+$CC src/vc/sbph.c src/vc/vcutils.c $CCFLAGS -o bin/vc/sbph
+$CC src/vc/ngram.c src/vc/vcutils.c $CCFLAGS -o bin/vc/ngram
+$CC src/vc/bow.c src/vc/vcutils.c $CCFLAGS -o bin/vc/bow
+$CC src/vc/osb.c src/vc/vcutils.c $CCFLAGS -o bin/vc/osb
+$CC src/vc/sbphmh.c src/vc/vcutils.c $CCFLAGS -o bin/vc/sbphmh
 
-cc src/cf/nbc.c -lm -Wall -o bin/cf/nbc
-cc src/cf/winnow.c -lm -Wall -o bin/cf/winnow
+$CC src/cf/nbc.c -lm $CCFLAGS -o bin/cf/nbc
+$CC src/cf/winnow.c -lm $CCFLAGS -o bin/cf/winnow
 
 # Prepare glueware
 cp src/*.py bin/
